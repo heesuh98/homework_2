d8c9b458eac0b5b0e78202f490b95974e01a8ec5
heesuh98 <heesuh98@naver.com>
2020-10-31 17:23:05 +0900

    add src

---
+++ src/vcfparser.py
@@ -0,0 +1,9 @@
+class VcfParser:
+	def __init__(self, str, set):
+		self.vcf_file = "str"
+		self.variants = "set"
+
+	def get_variants(self):
+		self.get_variants()
+
+	def compare_variants(self, other):
